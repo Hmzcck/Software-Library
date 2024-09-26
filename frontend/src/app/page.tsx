67664f96928847203b46a0e4c4aa3160ab4081f4
Hamza Çiçek <126699123+Hmzcck@users.noreply.github.com>
2024-09-26 15:14:54 +0300

    Add Footer component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
    <div>
     <Header/>
      <h1>Software Library</h1>
+     <Footer/>
    </div>
   );
 }
